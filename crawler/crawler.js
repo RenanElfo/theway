8bd08912e008290748fc1ceb945086c43db2004e
Elfo <renanmedeirosaguiar@gmail.com>
2025-05-19 21:35:10 -0300

    Web crawler now fetches points correctly

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,9 +1,13 @@
 import { JSDOM } from 'jsdom';
 
-import { languages, getPathWithLanguage } from './lang.js';
+import { getPathWithLanguage } from './lang.js';
+
+const NUMBER_OF_POINTS = 999;
 
 // Necessary delay to respect Crawl-delay specified in escriva.org/robots.txt
 const CRAWL_DELAY_MS = 10_000;
+// Using only pt-br for now.
+const LANGUAGES = ['pt-br']
 
 function sleep(delayMs) {
   const start = Date.now();
@@ -11,31 +15,37 @@ function sleep(delayMs) {
 }
 
 async function getHtmlAsText() {
-  languages.forEach((lang) => {
+  const languages = LANGUAGES;
+  languages.forEach(async (lang) => {
     console.log(`Crawling for language ${lang}`);
-    sleep(CRAWL_DELAY_MS);
-    return await fetch(getPathWithLanguage('pt-br') + '1')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error: ${response.status}`);
-        }
-        return response.text();
-      })
-      .then((text) => {
-        const dom = new JSDOM(text);
-        const document = dom.window.document;
-        const contentAsHtml = document.getElementById('contenido');
-        const paragraphsAsHtml = contentAsHtml.getElementsByTagName('p');
-        const contentAsHtmlArray = Array.prototype.slice.call(paragraphsAsHtml);
-        const point = contentAsHtmlArray
-          .map((p) => p.innerHTML)
-          .slice(0, -1)
-          .join('\n\n');
-        console.log(point);
-      });
+    for (let i = 0; i < 2; i++) {
+      console.log(`Crawling for point ${i}`);
+      sleep(CRAWL_DELAY_MS);
+      const r = await fetch(getPathWithLanguage(lang) + (i+1).toString())
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status},
+              ${getPathWithLanguage(lang) + i.toString()}
+            `);
+          }
+          return response.text();
+        })
+        .then((text) => {
+          const dom = new JSDOM(text);
+          const document = dom.window.document;
+          const contentAsHtml = document.getElementById('contenido');
+          const paragraphsAsHtml = contentAsHtml.getElementsByTagName('p');
+          const contentAsHtmlArray = Array.prototype.slice.call(paragraphsAsHtml);
+          const point = contentAsHtmlArray
+            .map((p) => p.innerHTML)
+            .slice(0, -1)
+            .join('\n\n');
+          return point;
+        });
+      console.log(r);
     }
-  );
+  });
 }
 
-console.log('html', getHtmlAsText());
+getHtmlAsText();
 
