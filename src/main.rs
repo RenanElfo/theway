0d6a816c0c87035ec0186bff9bfa8d73c6b31a94
Elfo <renanmedeirosaguiar@gmail.com>
2025-06-16 22:48:17 -0300

    Removed return from main

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ use format::format_text;
 mod point;
 use point::{Point};
 
-fn main() -> serde_json::Result<()> {
+fn main() {
     let point = Point::new();
 
     println!("{}. {}", point.number, point.subject);
     for paragraph in point.paragraphs {
         println!("\n{}", format_text(paragraph.clone(), 50));
     }
-
-    Ok(())
 }
