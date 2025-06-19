647d6b14b32056b1107b43083414b52a6809e634
Elfo <renanmedeirosaguiar@gmail.com>
2025-06-19 12:22:07 -0300

    Changed HTML italic tag for ANSI escape character

--- src/format.rs
+++ src/format.rs
@@ -1,7 +1,8 @@
 pub fn format_text(text: String, width: usize) -> String {
+    let italicized_text = convert_italic(&text);
     let mut out = String::new();
     let mut char_counter = 0;
-    for word in text.split_whitespace() {
+    for word in italicized_text.split_whitespace() {
         if char_counter + word.len() > width {
             out.push('\n');
             char_counter = 0;
@@ -11,7 +12,9 @@ pub fn format_text(text: String, width: usize) -> String {
         char_counter += word.len() + 1;
         out.push_str(word);
     }
-
     return out;
 }
 
+pub fn convert_italic(text: &str) -> String {
+    return text.replace("<i>", "\x1b[3m").replace("</i>", "\x1b[23m");
+}
